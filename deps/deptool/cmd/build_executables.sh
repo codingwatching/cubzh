328db1804aab843f9018bb96580af3399ef9a03a
Gaëtan de Villèle <gdevillele@proton.me>
2025-04-30 15:21:37 +0200

    deptool: improve dependency management & support libjolt

--- deps/deptool/cmd/build_executables.sh
+++ deps/deptool/cmd/build_executables.sh
@@ -2,19 +2,27 @@
 
 # Build executables for each platform
 
-# Build for macos
+# ---------------------------
+# macos
+# ---------------------------
 GOOS=darwin GOARCH=arm64 go build -o deptool_macos_arm64
 echo "✅ deptool_macos_arm64 built"
+
 GOOS=darwin GOARCH=amd64 go build -o deptool_macos_x86_64
 echo "✅ deptool_macos_x86_64 built"
 
-# Build for windows
+# ---------------------------
+# windows
+# ---------------------------
 GOOS=windows GOARCH=amd64 go build -o deptool_windows_x86_64.exe
 echo "✅ deptool_windows_x86_64.exe built"
 # GOOS=windows GOARCH=arm64 go build -o deptool_windows_arm64.exe
 
-# Build for linux
+# ---------------------------
+# linux
+# ---------------------------
 GOOS=linux GOARCH=arm64 go build -o deptool_linux_arm64
 echo "✅ deptool_linux_arm64 built"
+
 GOOS=linux GOARCH=amd64 go build -o deptool_linux_x86_64
 echo "✅ deptool_linux_x86_64 built"
--- deps/deptool/dependency.go
+++ deps/deptool/dependency.go
@@ -1,9 +1,11 @@
 package deptool
 
 import (
+	"fmt"
 	"os"
 	"path/filepath"
 	"slices"
+	"strings"
 )
 
 const ( // Supported dependencies
@@ -19,8 +21,6 @@ const ( // Supported dependencies
 	PlatformMacos   = "macos"
 	PlatformWindows = "windows"
 	PlatformLinux   = "linux"
-
-	// PlatformWeb     = "web"
 )
 
 var (
@@ -40,11 +40,60 @@ func constructDepArtifactsPath(depName, version, platform string) string {
 	return filepath.Join(depName, version, "prebuilt", platform)
 }
 
-// TODO: make the testing more robust
-func areDependencyFilesInstalled(depsDirPath, depName, version, platform string) (string, bool) {
-	// check if the dependency files exist
+// Check if a dependency is installed
+//
+// Return values:
+// - string: the full path to the dependency files
+// - bool: whether the dependency is installed
+// - error
+func areDependencyFilesInstalled(depsDirPath, depName, version, platform string) (string, bool, error) {
+	// fullDepPath is like: [...]/cubzh/deps/libpng/1.6.47/prebuilt/macos
 	fullDepPath := filepath.Join(depsDirPath, constructDepArtifactsPath(depName, version, platform))
+
+	// check if the directory exists
 	_, err := os.Stat(fullDepPath)
-	exists := err == nil
-	return fullDepPath, exists
+	if err != nil {
+		if os.IsNotExist(err) {
+			// directory does not exist
+			return fullDepPath, false, nil
+		}
+		// error
+		return "", false, fmt.Errorf("failed to check if directory exists: %w", err)
+	}
+
+	// directory exists (but it can be empty)
+
+	headerFileCount := 0
+	sourceFileCount := 0
+	libFileCount := 0
+
+	// walk the directory recursively and check if there are any files
+	err = filepath.Walk(fullDepPath, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		if !info.IsDir() { // found a file
+			if strings.HasSuffix(path, ".h") || strings.HasSuffix(path, ".hpp") {
+				headerFileCount++
+			} else if strings.HasSuffix(path, ".c") || strings.HasSuffix(path, ".cpp") {
+				sourceFileCount++
+			} else if strings.HasSuffix(path, ".a") || strings.HasSuffix(path, ".lib") {
+				libFileCount++
+			}
+		}
+
+		return nil
+	})
+	if err != nil {
+		return "", false, fmt.Errorf("failed to walk directory: %w", err)
+	}
+
+	// if no headers or (no libs and no sources) found,
+	// it means the dependency is not installed
+	if headerFileCount == 0 || (libFileCount == 0 && sourceFileCount == 0) {
+		return fullDepPath, false, nil
+	}
+
+	return fullDepPath, true, nil
 }
