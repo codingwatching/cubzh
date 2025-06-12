8ce816fbd402e6f9d914f4b704315ad24303e9e3
Gaëtan de Villèle <gdevillele@proton.me>
2025-06-12 23:46:54 +0200

    disable our own HTTP caching for iOS & macOS

--- deps/xptools/include/Macros.h
+++ deps/xptools/include/Macros.h
@@ -12,3 +12,12 @@
 #define VX_DISALLOW_COPY_AND_ASSIGN(TypeName) \
 TypeName( const TypeName& ) = delete;\
 TypeName& operator=( const TypeName& ) = delete;
+
+// Don't use our own HTTP caching on the following platforms:
+// - iOS & macOS (Apple's HTTP API is taking care of it)
+// - wasm (the web browser is taking care of it)
+#if !defined(__VX_PLATFORM_WASM) && !defined(__VX_PLATFORM_IOS) && !defined(__VX_PLATFORM_MACOS)
+#define __VX_USE_HTTP_CACHING 1
+#else
+#define __VX_USE_HTTP_CACHING 0
+#endif
