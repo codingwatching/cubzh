6e7d12a20382c7085b739d495d3ce5c6bf098dee
Gaëtan de Villèle <gdevillele@proton.me>
2025-06-21 10:59:41 +0200

    remove go vendor directory for docs webserver

--- lua/docs/webserver/vendor/github.com/gosimple/slug/slug.go
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:1eb9b3bde66382288a1d784a3fa7bc6aa6273ba3741e8658db18861ea0a86059
-size 6274
--- lua/docs/webserver/vendor/gopkg.in/yaml.v2/encode.go
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:6f842189d0530a22bd146824cf266e9c4fda56b9ba5186238c0bf539537f3a56
-size 9812
